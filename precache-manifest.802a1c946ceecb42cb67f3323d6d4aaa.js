392e1c13170375fe1dbd357b06c05d0cb7c3c819
forestfox128 <basiab217@gmail.com>
2019-05-27 21:50:27 +0200

    Updates

---
+++ precache-manifest.802a1c946ceecb42cb67f3323d6d4aaa.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0f0e66b4325f60aa651e806ef455cb5",
+    "url": "/forestfox128/index.html"
+  },
+  {
+    "revision": "c58f78eb724ad1e4aa3e",
+    "url": "/forestfox128/static/css/main.40782470.chunk.css"
+  },
+  {
+    "revision": "c6f0845a2cc4c7e6e63c",
+    "url": "/forestfox128/static/js/2.9b7897d9.chunk.js"
+  },
+  {
+    "revision": "c58f78eb724ad1e4aa3e",
+    "url": "/forestfox128/static/js/main.4cec81b1.chunk.js"
+  },
+  {
+    "revision": "7b9a3f36708a46f079e0",
+    "url": "/forestfox128/static/js/runtime~main.5d8b7581.js"
+  }
+]);
